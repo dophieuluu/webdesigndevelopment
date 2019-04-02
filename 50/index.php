df3b1aa411d48ef0ae2d72d60b005bf6bb7b0f16
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 11:18:21 +0500

    Add files via upload

---
+++ 50/index.php
@@ -0,0 +1,37 @@
+s<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="pricingTable-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="pricingTable">
+                    <div class="pricingTable-header">
+                        <span class="heading">
+                            <h3>standard plan</h3>
+                        </span>
+                        <span class="price-value">$<span>39</span><span class="month">/ month</span></span>
+                    </div>
+                    <div class="pricingContent">
+                        <ul>
+                            <li>Dedicated Hardware</li>
+                            <li>Virtualization-Ready</li>
+                            <li>Dedicated Support Team</li>
+                        </ul>
+                    </div><!-- /  CONTENT BOX-->
+                    <div class="pricingTable-sign-up"><!-- BUTTON BOX-->
+                        <a href="#" class="btn btn-default">Sign Up</a>
+                    </div><!-- BUTTON BOX-->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
