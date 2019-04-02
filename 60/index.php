d976b46ec35c1ce48d37003a160d1a26ba65ef7e
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 11:28:16 +0500

    Add files via upload

---
+++ 60/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="container">
+    <div class="row justify-content-center">
+<div class="col-md-12">
+<h1 style="margin-top: 20px; text-align: center; color: #000;">Pricing Table Styles</h1>
+</div>
+        <div class="col-md-4 col-sm-6">
+            <div class="pricingTable">
+                <div class="pricingTable-header">
+                    <div class="pricing-icon">
+                        <i class="fa fa-adjust"></i>
+                    </div>
+                    <h4 class="heading">Standard</h4>
+                </div>
+                <div class="price-value">
+                    <span class="currency">$</span><span>10.00</span><span class="month">monthly</span>
+                </div>
+                <div class="pricingContent">
+                    <ul>
+                        <li><b>50GB</b> Disk Space</li>
+                        <li><b>50</b> Email Accounts</li>
+                        <li><b>50GB</b> Monthly Bandwidth</li>
+                        <li><b>10</b> subdomains</li>
+                        <li><b>15</b> Domains</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+ 
+        <div class="col-md-4 col-sm-6">
+            <div class="pricingTable pink">
+                <div class="pricingTable-header">
+                    <div class="pricing-icon">
+                        <i class="fa fa-briefcase"></i>
+                    </div>
+                    <h4 class="heading">Business</h4>
+                </div>
+                <div class="price-value">
+                    <span class="currency">$</span><span>20.00</span><span class="month">monthly</span>
+                </div>
+                <div class="pricingContent">
+                    <ul>
+                        <li><b>60GB</b> Disk Space</li>
+                        <li><b>60</b> Email Accounts</li>
+                        <li><b>60GB</b> Monthly Bandwidth</li>
+                        <li><b>15</b> subdomains</li>
+                        <li><b>20</b> Domains</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+<p style="margin-top: 20px; text-align: center;  color: #000;">wwww.websitedesignndevelopment.com</p>
+</div>
+</div>
+</body>
+</html>
