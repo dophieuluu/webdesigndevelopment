5088f3d69f11ce649e78f9a4125c77e07ac37434
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 12:02:24 +0500

    Add files via upload

---
+++ 113/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css "/>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="demo">
+    <div class="container">
+       <div class="row justify-content-center">
+<div class="col-md-12">
+<h1 style="margin-top: 20px; text-align: center; color: #000;">Pricing Table Styles</h1>
+</div>
+            <div class="col-md-4 col-sm-6">
+                <div class="pricingTable">
+                    <svg viewBox="0 0 100 50" class="pricingTable-header">
+                        <path d="M 0,5 C 40,20 70,-5 100,5 L 100,43 C 60,27 20,58 0,40 Z" stroke="none" fill="rgba(0,0,0,0.5)"></path>
+                        <path d="M 0,5 C 40,20 70,-5 100,5 L 100,40 C 60,25 20,55 0,35 Z" stroke="none" fill="#0b816b"></path>
+                        <text x="17" y="30" fill="#fff" font-size="17">Standard</text>
+                    </svg>
+                    <div class="price-value">
+                        <span class="amount">10.00$</span>
+                        <span class="month">per month</span>
+                    </div>
+                    <ul class="pricing-content">
+                        <li> <b>50GB</b> Disk Space</li>
+                        <li> <b>50</b> Email Accounts</li>
+                        <li> <b>50GB</b> Bandwidth</li>
+                        <li> <b>10</b> Subdomains</li>
+                        <li> <b>15</b> Domains</li>
+                    </ul>
+                    <div class="pricingTable-signup">
+                        <svg x="0px" y="0px" width="180px" viewBox="0 0 280 168" class="signup">
+                            <a href="#">
+                                <path fill="#0B826C" stroke="#0b826c" stroke-width="0.78" d="M265.73,81.983l-86.78,67.33c-22.101,17.36-45.24,22.101-70.49,2.11 l-88.89-68.38c-28.4-22.09,1.58-81.53,24.72-81.53l195.14,1.05C264.68,3.093,293.09,60.953,265.73,81.983z"></path>
+                                <text transform="matrix(0.9911 0 0 1 86.5449 52.4736)">
+                                    <tspan x="3" y="0" fill="#ffffff" font-size="55">BUY</tspan>
+                                    <tspan x="-11.359" y="57.313" fill="#ffffff" font-size="55">NOW</tspan>
+                                </text>
+                            </a>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="pricingTable blue">
+                    <svg viewBox="0 0 100 50" class="pricingTable-header">
+                        <path d="M 0,5 C 40,20 70,-5 100,5 L 100,43 C 60,27 20,58 0,40 Z" stroke="none" fill="rgba(0,0,0,0.5)"></path>
+                        <path d="M 0,5 C 40,20 70,-5 100,5 L 100,40 C 60,25 20,55 0,35 Z" stroke="none" fill="#1372a8"></path>
+                        <text x="17" y="30" fill="#fff" font-size="17">Business</text>
+                    </svg>
+                    <div class="price-value">
+                        <span class="amount">20.00$</span>
+                        <span class="month">per month</span>
+                    </div>
+                    <ul class="pricing-content">
+                        <li> <b>60GB</b> Disk Space</li>
+                        <li> <b>60</b> Email Accounts</li>
+                        <li> <b>60GB</b> Bandwidth</li>
+                        <li> <b>15</b> Subdomains</li>
+                        <li> <b>20</b> Domains</li>
+                    </ul>
+                    <div class="pricingTable-signup">
+                        <svg x="0px" y="0px" width="180px" viewBox="0 0 280 168" class="signup">
+                            <a href="#">
+                                <path fill="#1372a8" stroke="#1372a8" stroke-width="0.78" d="M265.73,81.983l-86.78,67.33c-22.101,17.36-45.24,22.101-70.49,2.11 l-88.89-68.38c-28.4-22.09,1.58-81.53,24.72-81.53l195.14,1.05C264.68,3.093,293.09,60.953,265.73,81.983z"></path>
+                                <text transform="matrix(0.9911 0 0 1 86.5449 52.4736)">
+                                    <tspan x="3" y="0" fill="#ffffff" font-size="55">BUY</tspan>
+                                    <tspan x="-11.359" y="57.313" fill="#ffffff" font-size="55">NOW</tspan>
+                                </text>
+                            </a>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+<p style="margin-top: 20px; text-align: center;  color: #000;">wwww.websitedesignndevelopment.com</p>
+</div>
+</div>
+</body>
+</html>
