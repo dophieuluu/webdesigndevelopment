972d47dbc57e66ad965ec7e6ee7a7f9202aecc0c
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 12:02:26 +0500

    Add files via upload

---
+++ 114/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css "/>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="demo">
+    <div class="container">
+     <div class="row justify-content-center">
+<div class="col-md-12">
+<h1 style="margin-top: 20px; text-align: center; color: #000;">Pricing Table Styles</h1>
+</div>
+            <div class="col-md-4 col-sm-6">
+                <div class="pricingTable">
+                    <div class="pricingTable-header">
+                        <svg x="0px" y="0px" width="350px" height="230px" viewBox="0 5 350 200">
+                            <g>
+                                <path fill="#D7182A" d="M58.377,62.185c11.838-16.874,24.94-20.129,43.466-18.93C102.476,24.326,166.999,15.932,175.042,0
+                c13.916,18.501,71.028,22.526,72.926,43.769c18.436-3.169,39.31,6.852,42.563,17.131c76.091-0.171,82.236,75.631,0.633,78.715
+                c-9.939,13.106-23.496,19.87-43.287,17.988C244.805,176.36,190.766,183.04,174.59,200c-13.285-17.216-71.12-23.04-71.934-42.482
+                c-18.795,2.228-33.888-4.284-44.732-18.502C-31.088,134.903-6.688,54.989,58.377,62.185L58.377,62.185z"></path>
+                                <path fill="none" stroke="#FFFFFF" stroke-width="0" stroke-linecap="square" stroke-miterlimit="10" d="M58.377,62.185
+                c11.838-16.874,24.94-20.129,43.466-18.93C102.476,24.326,166.999,15.932,175.042,0c13.916,18.501,71.028,22.526,72.926,43.769
+                c18.436-3.169,39.31,6.852,42.563,17.131c76.091-0.171,82.236,75.631,0.633,78.715c-9.939,13.106-23.496,19.87-43.287,17.988
+                C244.805,176.36,190.766,183.04,174.59,200c-13.285-17.216-71.12-23.04-71.934-42.482c-18.795,2.228-33.888-4.284-44.732-18.502
+                C-31.088,134.903-6.688,54.989,58.377,62.185L58.377,62.185z"></path>
+                                <path fill="none" stroke="#FFFFFF" stroke-width="3.54" d="M112.326,54.133c-4.519-21.67,51.78-25.096,63.167-39.401
+            c12.741,16.617,67.686,18.501,61.993,39.829c18.615-4.54,45.183,6.509,46.81,17.216c68.048,0.771,75.367,54.562,0.542,57.303
+            c-5.873,13.104-26.024,20.128-46.9,17.217c1.898,22.182-47.263,25.096-63.076,39.144c-12.201-13.963-66.782-18.074-63.529-39.403
+            c-17.44,2.914-37.322,0.26-46.268-17.728c-78.982-0.173-61.27-62.014,1.716-55.761C70.305,53.019,100.036,52.676,112.326,54.133
+            L112.326,54.133z"></path>
+                            </g>
+                        </svg>
+                        <h3 class="title">Standard</h3>
+                        <div class="price-value">
+                            <span class="amount">$10.00</span>
+                            <span class="month">per month</span>
+                        </div>
+                    </div>
+                    <ul class="pricing-content">
+                        <li><b>50GB</b> Disk Space</li>
+                        <li><b>50</b> Email Accounts</li>
+                        <li><b>50GB</b> Bandwidth</li>
+                        <li><b>10</b> Subdomains</li>
+                        <li><b>15</b> Domains</li>
+                    </ul>
+                    <a href="#" class="pricingTable-signup">Sign Up</a>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="pricingTable darkblue">
+                    <div class="pricingTable-header">
+                        <svg x="0px" y="0px" width="350px" height="230px" viewBox="0 5 350 200">
+                            <g>
+                                <path fill="#D7182A" d="M58.377,62.185c11.838-16.874,24.94-20.129,43.466-18.93C102.476,24.326,166.999,15.932,175.042,0
+                c13.916,18.501,71.028,22.526,72.926,43.769c18.436-3.169,39.31,6.852,42.563,17.131c76.091-0.171,82.236,75.631,0.633,78.715
+                c-9.939,13.106-23.496,19.87-43.287,17.988C244.805,176.36,190.766,183.04,174.59,200c-13.285-17.216-71.12-23.04-71.934-42.482
+                c-18.795,2.228-33.888-4.284-44.732-18.502C-31.088,134.903-6.688,54.989,58.377,62.185L58.377,62.185z"></path>
+                                <path fill="none" stroke="#FFFFFF" stroke-width="0" stroke-linecap="square" stroke-miterlimit="10" d="M58.377,62.185
+                c11.838-16.874,24.94-20.129,43.466-18.93C102.476,24.326,166.999,15.932,175.042,0c13.916,18.501,71.028,22.526,72.926,43.769
+                c18.436-3.169,39.31,6.852,42.563,17.131c76.091-0.171,82.236,75.631,0.633,78.715c-9.939,13.106-23.496,19.87-43.287,17.988
+                C244.805,176.36,190.766,183.04,174.59,200c-13.285-17.216-71.12-23.04-71.934-42.482c-18.795,2.228-33.888-4.284-44.732-18.502
+                C-31.088,134.903-6.688,54.989,58.377,62.185L58.377,62.185z"></path>
+                                <path fill="none" stroke="#FFFFFF" stroke-width="3.54" d="M112.326,54.133c-4.519-21.67,51.78-25.096,63.167-39.401
+            c12.741,16.617,67.686,18.501,61.993,39.829c18.615-4.54,45.183,6.509,46.81,17.216c68.048,0.771,75.367,54.562,0.542,57.303
+            c-5.873,13.104-26.024,20.128-46.9,17.217c1.898,22.182-47.263,25.096-63.076,39.144c-12.201-13.963-66.782-18.074-63.529-39.403
+            c-17.44,2.914-37.322,0.26-46.268-17.728c-78.982-0.173-61.27-62.014,1.716-55.761C70.305,53.019,100.036,52.676,112.326,54.133
+            L112.326,54.133z"></path>
+                            </g>
+                        </svg>
+                        <h3 class="title">Business</h3>
+                        <div class="price-value">
+                            <span class="amount">$20.00</span>
+                            <span class="month">per month</span>
+                        </div>
+                    </div>
+                    <ul class="pricing-content">
+                        <li><b>60GB</b> Disk Space</li>
+                        <li><b>60</b> Email Accounts</li>
+                        <li><b>60GB</b> Bandwidth</li>
+                        <li><b>15</b> Subdomains</li>
+                        <li><b>20</b> Domains</li>
+                    </ul>
+                    <a href="#" class="pricingTable-signup">Sign Up</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+<p style="margin-top: 20px; text-align: center;  color: #000;">wwww.websitedesignndevelopment.com</p>
+</div>
+</div>
+</body>
+</html>
