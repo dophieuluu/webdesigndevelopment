3b12cacea3cb1ec19170490a3faca98a2a4a0d7e
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 10:52:58 +0500

    Add files via upload

---
+++ 31/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="container">
+    <div class="row justify-content-center">
+<div class="col-md-12">
+<h1 style="margin-top: 20px; text-align: center; color: #000;">Pricing Table Styles</h1>
+</div>
+        <div class="col-sm-4">
+            <div class="pricingTable">
+                <div class="pricingTable-header">
+                    <span class="price-value"><span class="currency">$</span>20<span class="mo">per month</span></span>
+                    <span class="heading">
+                        <h3>Standard</h3>
+                    </span>
+                </div>
+                <div class="pricingContent">
+                    <ul>
+                        <li><a href="#"><b>50GB</b> Disk Space</a></li>
+                        <li><a href="#"><b>50</b> Email Accounts</a></li>
+                        <li><a href="#"><b>50GB</b> Monthly Bandwidth</a></li>
+                        <li><a href="#"><b>50</b> Domains</a></li>
+                        <li><a href="#"><b>Unlimited</b> Subdomains</a></li>
+                    </ul>
+                </div><!-- /  CONTENT BOX-->
+                <div class="pricingTable-sign-up">
+                    <a href="#" class="btn btn-block btn-default">sign up</a>
+                </div><!-- BUTTON BOX-->
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+<p style="margin-top: 20px; text-align: center;  color: #000;">wwww.websitedesignndevelopment.com</p>
+</div>
+</div>
+</body>
+</html>
