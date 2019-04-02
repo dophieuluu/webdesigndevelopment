b9c775791d51aedd4776ad725f9ed01366dcdd0d
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 11:31:12 +0500

    Add files via upload

---
+++ 55/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="container">
+    <div class="row justify-content-center">
+<div class="col-md-12">
+<h1 style="margin-top: 20px; text-align: center; color: #000;">Pricing Table Styles</h1>
+</div>
+        <div class="col-md-4 col-sm-6">
+            <div class="pricingTable purple">
+                <div class="pricingTable-header">
+                    <h3>Standard</h3>
+                    <span>Lorem ipsum dolor</span>
+                </div>
+                <div class="pricing-plans">
+                    <span class="price-value"><i class="fa fa-usd"></i><span>10.00</span></span>
+                    <span class="month">/month</span>
+                </div>
+                <div class="pricingContent">
+                    <ul>
+                        <li><b>50GB</b> Disk Space</li>
+                        <li><b>50</b> Email Accounts</li>
+                        <li><b>50GB</b> Monthly Bandwidth</li>
+                        <li><b>10</b> subdomains</li>
+                        <li><b>15</b> Domains</li>
+                    </ul>
+                </div><!-- CONTENT BOX-->
+                <div class="pricingTable-sign-up">
+                    <a href="#" class="btn btn-block">sign up</a>
+                </div><!-- BUTTON BOX-->
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+<p style="margin-top: 20px; text-align: center;  color: #000;">wwww.websitedesignndevelopment.com</p>
+</div>
+</div>
+</body>
+</html>
