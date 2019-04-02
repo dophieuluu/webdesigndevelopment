7705f44eeeab35db831c89a1da93f52afed72405
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 11:58:29 +0500

    Add files via upload

---
+++ 110/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="demo">
+    <div class="container">
+        <div class="row justify-content-center">
+<div class="col-md-12">
+<h1 style="margin-top: 20px; text-align: center; color: #000;">Pricing Table Styles</h1>
+</div>
+            <div class="col-md-4 col-sm-6">
+                <div class="pricingTable">
+                    <div class="pricingTable-header">
+                        <h3 class="title">Standard</h3>
+                        <span class="sub-title">Lorem ipsum</span>
+                        <span class="year">Pay only <br>$110/year</span>
+                    </div>
+                    <div class="price-value">
+                        <div class="value">
+                            <span class="currency">$</span>
+                            <span class="amount">10.<span>99</span></span>
+                            <span class="month">/month</span>
+                        </div>
+                    </div>
+                    <ul class="pricing-content">
+                        <li>50GB Disk Space</li>
+                        <li>50 Email Accounts</li>
+                        <li>50GB Monthly Bandwidth</li>
+                        <li class="disable">10 Subdomains</li>
+                        <li class="disable">15 Domains</li>
+                    </ul>
+                    <a href="#" class="pricingTable-signup">Sign up</a>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="pricingTable purple">
+                    <div class="pricingTable-header">
+                        <h3 class="title">Business</h3>
+                        <span class="sub-title">Lorem ipsum</span>
+                        <span class="year">Pay only <br>$220/year</span>
+                    </div>
+                    <div class="price-value">
+                        <div class="value">
+                            <span class="currency">$</span>
+                            <span class="amount">20.<span>99</span></span>
+                            <span class="month">/month</span>
+                        </div>
+                    </div>
+                    <ul class="pricing-content">
+                        <li>60GB Disk Space</li>
+                        <li>60 Email Accounts</li>
+                        <li>60GB Monthly Bandwidth</li>
+                        <li>15 Subdomains</li>
+                        <li class="disable">20 Domains</li>
+                    </ul>
+                    <a href="#" class="pricingTable-signup">Sign up</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+<p style="margin-top: 20px; text-align: center;  color: #000;">wwww.websitedesignndevelopment.com</p>
+</div>
+</div>
+</body>
+</html>
