c2e7bc913a88af0b660e0ba30ddfea18bea17f38
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 11:28:12 +0500

    Add files via upload

---
+++ 59/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="demo">
+    <div class="container">
+       <div class="row justify-content-center">
+<div class="col-md-12">
+<h1 style="margin-top: 20px; text-align: center; color: #000;">Pricing Table Styles</h1>
+</div>
+            <div class="col-md-3 col-sm-6">
+                <div class="pricingTable">
+                    <div class="pricingTable-header">
+                        <span class="price-icon"></span>
+                        <span class="price-value">$10<span class="month">monthly</span></span>
+                        <h3 class="heading">Standard</h3>
+                    </div>
+                    <div class="pricingContent">
+                        <ul>
+                            <li><i class="fa fa-check"></i>50GB Disk Space</li>
+                            <li><i class="fa fa-check"></i>50 Email Accounts</li>
+                            <li><i class="fa fa-check"></i>10 subdomains</li>
+                            <li><i class="fa fa-check"></i>15 Domains</li>
+                            <li><i class="fa fa-check"></i>50GB Monthly Bandwidth</li>
+                        </ul>
+                    </div><!-- /  CONTENT BOX-->
+                    <div class="pricingTable-signup">
+                        <a href="#"><i class="fa fa-shopping-cart"></i> order now</a>
+                    </div>
+                </div><!-- BUTTON BOX-->
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+<p style="margin-top: 20px; text-align: center;  color: #000;">wwww.websitedesignndevelopment.com</p>
+</div>
+</div>
+</body>
+</html>
