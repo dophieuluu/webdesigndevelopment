ed818f9016342a364ea433fc9d3cc1e4026e42d7
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 10:59:57 +0500

    Add files via upload

---
+++ 44/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title></title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="pricingTable">
+                <div class="pricingTable-header">
+                    <h4>Standard</h4>
+                    <span>Lorem ipsum dolor sit</span>
+                    <span class="price-value">$<span>10</span></span>
+                </div>
+                <div class="pricingContent">
+                    <ul>
+                        <li><b>50GB</b> Disk Space</li>
+                        <li><b>50</b> Email Accounts</li>
+                        <li><b>50GB</b> Monthly Bandwidth</li>
+                        <li><b>10</b> subdomains</li>
+                        <li><b>50</b> Domains</li>
+                    </ul>
+                </div><!-- /  CONTENT BOX-->
+                <div class="pricingTable-sign-up"><!-- BUTTON BOX-->
+                    <a href="#" class="btn btn-block btn-default">sign up</a>
+                </div><!-- BUTTON BOX-->
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
