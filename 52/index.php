021e6750640fc7ccf0f74a94194beb442dc1b5ec
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 11:26:06 +0500

    Add files via upload

---
+++ 52/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title></title>
+</head>
+<body style="background-color: #6e006a;">
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="pricingTable">
+                <div class="pricingTable-header">
+                    <span class="heading">Standard</span>
+                    <span class="price-value"><i class="fa fa-usd"></i>10</span>
+                    <span class="subtitle">Lorem ipsum dolor</span>
+                </div>
+                <div class="pricingContent">
+                    <ul>
+                        <li><b>50GB</b> Disk Space</li>
+                        <li><b>50</b> Email Accounts</li>
+                        <li><b>50GB</b> Monthly Bandwidth</li>
+                        <li><b>10</b> subdomains</li>
+                        <li><b>15</b> Domains</li>
+                    </ul>
+                </div><!-- /  CONTENT BOX-->
+                <div class="pricingTable-sign-up"><!-- BUTTON BOX-->
+                    <a href="#" class="btn btn-default">buy now</a>
+                </div><!-- BUTTON BOX-->
+            </div>
+        </div>
+    </div>
+</div
+</body>
+</html>
