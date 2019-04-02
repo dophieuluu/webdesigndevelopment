272b7d9d708f3b6ad214fb90994a14c4ee43e1f9
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 11:34:10 +0500

    Add files via upload

---
+++ 69/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="container">
+    <div class="row justify-content-center">
+<div class="col-md-12">
+<h1 style="margin-top: 20px; text-align: center; color: #000;">Pricing Table Styles</h1>
+</div>
+        <div class="col-md-4 col-sm-6">
+            <div class="pricingTable">
+                <div class="price-Value">
+                    <span class="currency">$</span>10.00
+                </div>
+                <div class="pricing-content">
+                    <h3 class="title">Standard</h3>
+                    <ul>
+                        <li>50GB Disk Space</li>
+                        <li>50 Email Accounts</li>
+                        <li>50GB Monthly Bandwidth</li>
+                        <li>10 Subdomains</li>
+                        <li>15 Domains</li>
+                    </ul>
+                </div>
+                <div class="pricingTable-signup">
+                    <a href="#">SIGN IN</a>
+                </div>
+            </div>
+        </div>
+ 
+        <div class="col-md-4 col-sm-6">
+            <div class="pricingTable table2">
+                <div class="price-Value">
+                    <span class="currency">$</span>20.00
+                </div>
+                <div class="pricing-content">
+                    <h3 class="title">Business</h3>
+                    <ul>
+                        <li>60GB Disk Space</li>
+                        <li>60 Email Accounts</li>
+                        <li>60GB Monthly Bandwidth</li>
+                        <li>15 Subdomains</li>
+                        <li>20 Domains</li>
+                    </ul>
+                </div>
+                <div class="pricingTable-signup">
+                    <a href="#">SIGN IN</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+<p style="margin-top: 20px; text-align: center;  color: #000;">wwww.websitedesignndevelopment.com</p>
+</div>
+</div>
+</body>
+</html>
