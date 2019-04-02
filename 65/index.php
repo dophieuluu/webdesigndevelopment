4831e3b60d525225743d7d1ef882d0c654635023
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 11:33:45 +0500

    Add files via upload

---
+++ 65/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="container">
+   <div class="row justify-content-center">
+<div class="col-md-12">
+<h1 style="margin-top: 20px; text-align: center; color: #000;">Pricing Table Styles</h1>
+</div>
+        <div class="col-md-4 col-sm-6">
+            <div class="pricingTable">
+                <div class="pricingTable-header">
+                    <h3 class="heading">Standard</h3>
+                    <span class="subtitle">Lorem ipsum dolor sit amet</span>
+                </div>
+                <div class="price-value">
+                    <span class="month">Per Month</span>
+                    <span class="amount"><span class="currency">$</span>10</span>
+                </div>
+                <div class="pricing-content">
+                    <ul>
+                        <li><i class="fa fa-check"></i> 50GB Disk Space</li>
+                        <li><i class="fa fa-check"></i> 50 Email Accounts</li>
+                        <li><i class="fa fa-check"></i> 50GB Monthly Bandwidth</li>
+                        <li><i class="fa fa-check"></i> 10 Subdomains</li>
+                        <li><i class="fa fa-check"></i> 15 Domains</li>
+                    </ul>
+                </div>
+                <a href="#" class="read">sign up</a>
+            </div>
+        </div>
+ 
+        <div class="col-md-4 col-sm-6">
+            <div class="pricingTable middle">
+                <div class="pricingTable-header">
+                    <h3 class="heading">Business</h3>
+                    <span class="subtitle">Lorem ipsum dolor sit amet</span>
+                </div>
+                <div class="price-value">
+                    <span class="month">Per Month</span>
+                    <span class="amount"><span class="currency">$</span>20</span>
+                </div>
+                <div class="pricing-content">
+                    <ul>
+                        <li><i class="fa fa-check"></i> 60GB Disk Space</li>
+                        <li><i class="fa fa-check"></i> 60 Email Accounts</li>
+                        <li><i class="fa fa-check"></i> 60GB Monthly Bandwidth</li>
+                        <li><i class="fa fa-check"></i> 15 Subdomains</li>
+                        <li><i class="fa fa-check"></i> 20 Domains</li>
+                    </ul>
+                </div>
+                <a href="#" class="read">sign up</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+<p style="margin-top: 20px; text-align: center;  color: #000;">wwww.websitedesignndevelopment.com</p>
+</div>
+</div>
+</body>
+</html>
