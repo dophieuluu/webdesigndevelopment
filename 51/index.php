717cdf6f8c598efc19440555dd9e100c51a19d77
webdesignndevelopment <49180243+webdesignndevelopment@users.noreply.github.com>
2019-04-02 11:21:57 +0500

    Add files via upload

---
+++ 51/index.php
@@ -0,0 +1,38 @@
+s<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="pricingTable">
+                <div class="pricingTable-header">
+                    <span class="heading">
+                        <h3>Standard</h3>
+                        <span>Lorem ipsum</span>
+                    </span>
+                    <span class="price-value">$<span>10.9</span><span class="month">monthly</span><span class="special">join</span></span>
+                </div>
+                <div class="pricingContent">
+                    <ul>
+                        <li><i class="fa fa-cogs"></i>50GB Disk Space</li>
+                        <li><i class="fa fa-diamond"></i>50 Email Accounts</li>
+                        <li><i class="fa fa-paper-plane"></i>50GB Monthly Bandwidth</li>
+                        <li><i class="fa fa-heart"></i>50 Domains</li>
+                        <li><i class="fa fa-globe"></i>Unlimited Subdomains</li>
+                    </ul>
+                </div><!-- /  CONTENT BOX-->
+                <div class="pricingTable-description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium error facere laboriosam maiores minus modi officiis quaerat!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
